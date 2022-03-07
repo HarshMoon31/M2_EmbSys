b9f9a84d3ce0c58fa05068c56d6f54bc55385572
HARSH MOON <98867723+HarshMoon31@users.noreply.github.com>
2022-03-07 09:29:46 +0530

    Create Activity_2.h

---
+++ Project/3_Implimentation/inc/Activity_2.h
@@ -0,0 +1,17 @@
+/**
+ * @file Activity_2.h
+ * @author Harsh R Moon
+ * @brief ADC covesrion
+ * @version 0.1
+ * @date 2022-02-04
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef ACTIVITY2_H_INCLUDED
+#define ACTIVITY2_H_INCLUDED
+
+void InitADC();
+uint16_t ReadADC(uint8_t ch);
+
+#endif

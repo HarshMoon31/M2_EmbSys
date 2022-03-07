e87c2e338b232991ae251e53d51b8aaadd771edd
HARSH MOON <98867723+HarshMoon31@users.noreply.github.com>
2022-03-07 09:30:22 +0530

    Create Activity_3.h

---
+++ Project/3_Implimentation/inc/Activity_3.h
@@ -0,0 +1,17 @@
+/**
+ * @file Activity_3.h
+ * @author Harsh R Moon
+ * @brief timer and pwm configuration
+ * @version 0.1
+ * @date 2022-02-03
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef ACTIVITY3_H_INCLUDED
+#define ACTIVITY3_H_INCLUDED
+
+void config_timer(void);
+void pwm_output(void);
+
+#endif
